1083fed981ada63efb18c90e41aa0a23ad79645e
a-xiewenbin <1056481167@qq.com>
2019-01-18 18:15:43 +0800

    file commit

--- admin-server/src/main/java/com/spring/boot/admin/config/SecuritySecureConfig.java
+++ admin-server/src/main/java/com/spring/boot/admin/config/SecuritySecureConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-
 @Configuration
 public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
     private final String adminContextPath;
 
     public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
