f80ef6edeea5158d6af5c619b8cfc826a97cc9ee
a-xiewenbin <1056481167@qq.com>
2019-01-18 15:27:46 +0800

    commit file

---
+++ admin-server/src/main/java/com/spring/boot/admin/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.spring.boot.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //开启eurke client的功能。
+@EnableAdminServer //开启admin server的功能
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
+
